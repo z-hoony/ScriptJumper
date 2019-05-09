43e570a19f72806a2ab8752e05c5b34b8287497c
hoony <hoony@hoony.org>
2019-05-09 21:23:18 +0900

    init project

---
+++ ScriptJumper/ScriptJumper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScriptJumper
+//
+//  Created by 현지훈 on 09/05/2019.
+//  Copyright © 2019 현지훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
